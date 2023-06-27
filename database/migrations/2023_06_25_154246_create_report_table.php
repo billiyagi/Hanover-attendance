ed80fe0bc84e7f12d56155ff7dc19dc74848c2e0
Febry Billiyagi <89958256+billiyagi@users.noreply.github.com>
2023-06-27 21:05:22 +0700

    Update 2023_06_25_154246_create_report_table.php

--- database/migrations/2023_06_25_154246_create_report_table.php
+++ database/migrations/2023_06_25_154246_create_report_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamp('range_start');
             $table->timestamp('range_end');
             $table->timestamps();
-           
         });
     }
 
